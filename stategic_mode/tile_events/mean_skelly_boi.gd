7c1c33baf2d2524e3ab2f6d7caded4b04b7bc2ff
Konstantin <kobelevkk@yandex.ru>
2024-04-04 13:01:00 +0500

    non-collectables are now loose

--- stategic_mode/tile_events/mean_skelly_boi.gd
+++ stategic_mode/tile_events/mean_skelly_boi.gd
@@ -4,6 +4,8 @@ extends "res://stategic_mode/tile_events/TileEvent.gd"
 @onready var game: Game = get_tree().root.get_child(0)
 @onready var inventory: Inventory = preload("res://inventory/global_inventory.tres")
 
+@export var enemies: Array[Unit]
+
 func activate():
 	inventory.set_def_chars()
 	var characters = inventory.characters
