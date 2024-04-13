a16daa2f7fa3b6a71308afb59125d019f6443850
Permyak_Logy <maxsika2004@mail.ru>
2024-04-13 20:57:51 +0500

    fixup

--- tactical_mode/base_unit/Skeleton/Skeleton.gd
+++ tactical_mode/base_unit/Skeleton/Skeleton.gd
@@ -1,8 +1,4 @@
-extends "res://tactical_mode/base_unit/unit.gd"
-
-class_name Skeleton
-
-@onready var tile_map = $"../TileMap"
+class_name Skeleton extends Unit
 
 var current_id_path: Array = []
 signal walk_finished
@@ -16,7 +12,7 @@ func _physics_process(delta):
 	if current_id_path.is_empty():
 		return
 		
-	var target_position = tile_map.map_to_local(current_id_path.front())
+	var target_position = $"../TileMap".map_to_local(current_id_path.front())
 
 	global_position = global_position.move_toward(target_position, 3)
 	if global_position == target_position:    
