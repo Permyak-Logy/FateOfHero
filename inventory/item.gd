e664a27427c18f3437bcdab4e9d50b2c0d37eab3
Void <pavel_sendv1k@mail.ru>
2024-04-28 13:28:23 +0500

    fixup

--- inventory/item.gd
+++ inventory/item.gd
@@ -5,6 +5,10 @@ class_name Item
 @export var name: String
 @export var texture: Texture2D
 @export var max_stack: int
+@export var recipe: Array[Item]
+@export var price: int
+
+@export var descript : String
 
 static func less(a: Item, b: Item):
 	return a.name < b.name
