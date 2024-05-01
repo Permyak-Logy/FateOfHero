dea1eb1463659fd6599e9c942950872f3fc1335e
Permyak_Logy <maxsika2004@mail.ru>
2024-05-01 16:56:08 +0500

    Save

--- tactical_mode/unit/components/ExpirienceComponent.gd
+++ tactical_mode/unit/components/ExpirienceComponent.gd
@@ -5,3 +5,11 @@ var expirience: int = 0
 
 func get_exp_to_next_lvl() -> int:
 	return 100
+
+func add_exp(_exp: int):
+	expirience += _exp
+	while expirience >= get_exp_to_next_lvl():
+		level_up()
+
+func level_up():
+	pass
