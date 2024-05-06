f7515adbcd17b2ae0fc47158633052f8b9f10459
Void <pavel_sendv1k@mail.ru>
2024-05-06 15:36:54 +0500

    исправил ошибки

--- city_mode/Cities/Geraldina/Geraldina.gd
+++ city_mode/Cities/Geraldina/Geraldina.gd
@@ -3,21 +3,12 @@ class_name Geraldina extends City
 #@onready var inventory: Inventory = preload("res://inventory/global_inventory.tres")
 
 func _on_button_exit_pressed():
-	game.to_strat_mode()
+
+	leave_city.emit()
 
 func _on_button_crafting_pressed():
-	get_tree().change_scene_to_file("res://city_mode/crafting/crafting.tscn")
-
+	get_tree().change_scene_to_file("res://city_mode/Cities/Geraldina/crafting/crafting.tscn")
 
 func _on_button_trading_pressed():
-	get_tree().change_scene_to_file("res://city_mode/trading/trading.tscn")
+	get_tree().change_scene_to_file("res://city_mode/Cities/Geraldina/trading/trading.tscn")
 #@onready var inventory: InventoryGUI = $InventoryGUI
-
-
-#func _input(event):
-#	if event.is_action_pressed("inv_button"): 
-#		if not inventory.is_open:
-#			inventory.open()
-#		else:
-#			inventory.close()
-
--- city_mode/Cities/Geraldina/crafting/crafting.gd
+++ city_mode/Cities/Geraldina/crafting/crafting.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
-
 func _on_button_exit_crafting_pressed():
-	get_tree().change_scene_to_file("res://city_mode/CityMap.tscn")
+	get_tree().change_scene_to_file("res://city_mode/Cities/Geraldina/Geraldina.tscn")
