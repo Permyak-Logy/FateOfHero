cca0eec2a562518f5aa34589894f124dfcbb79ca
Konstantin <kobelevkk@yandex.ru>
2024-05-10 14:57:46 +0500

    main menu reformat

--- GUI/main_menu/main_menu.gd
+++ GUI/main_menu/main_menu.gd
@@ -5,19 +5,20 @@ class_name MainMenu extends Control
 @onready var exit_button: Button = $Menu/Exit
 @onready var save_menu_container: Control = $SaveMenuComtainer
 
-var SaveMenuRes = preload("res://GUI/main_menu/save_menu.tscn") 
 
 func show_save_menu(saves: Array[PackedScene]):
-	var save_menu: SaveMenu = SaveMenuRes.instantiate()
-	save_menu_container.add_child(save_menu)
-	save_menu.prep(saves)
+	pass
+
+func _on_continue_pressed():
+	pass
 
 func _on_campaign_pressed():
-	show_save_menu([])
+	pass
 
 func _on_infinite_pressed():
-	show_save_menu([])
-
+	pass
 
 func _on_exit_pressed():
 	pass
+
+
