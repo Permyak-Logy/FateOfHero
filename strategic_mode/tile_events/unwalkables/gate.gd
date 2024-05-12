0dbf6ae230fe7a14fa98c8026fe1bfc5150ac4c0
Konstantin <kobelevkk@yandex.ru>
2024-05-12 18:06:19 +0500

    fixup

--- strategic_mode/tile_events/unwalkables/gate.gd
+++ strategic_mode/tile_events/unwalkables/gate.gd
@@ -6,10 +6,12 @@ func _ready():
 	strat_map.strat_map_loaded.connect(snap_to_grid)
 
 func open():
+	collider.disabled = true
 	strat_map.tilemap.set_walkable(map_pos, true)
 	sprite.frame = 1
 
 func close():
+	collider.disabled = false
 	strat_map.tilemap.set_walkable(map_pos, false)
 	sprite.frame = 0
 
