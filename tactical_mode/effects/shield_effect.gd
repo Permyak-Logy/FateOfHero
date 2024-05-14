a05ab60b0642a85a20eac8143532f3a86ac1472e
Permyak_Logy <maxsika2004@mail.ru>
2024-05-14 16:08:34 +0500

    Добавлена Полудница с почти всеми анимациями, релизован эффект горения

--- tactical_mode/effects/shield_effect.gd
+++ tactical_mode/effects/shield_effect.gd
@@ -1,21 +1,27 @@
 class_name ShieldEffect extends Effect
 
-@export var power: float = 0
+@export var power: ModValue
+@export var scaling_type: Mod.Type
+
+var strength: float
+var scale: float:
+	get:
+		return (instigator as Unit).stat_from_type(scaling_type).cur()
 
 func update_on_damage(_damage: float, _instigator: Node = null) -> float:
-	var diff = min(power, _damage)
+	var diff = min(strength, _damage)
 	_damage -= diff
-	power -= diff
+	strength -= diff
 	get_map().write_info(
 		"=> Заблокировано щитом " + str(int(diff)) + " урона (" + owner.unit_name + ")"
 	)
-	if power <= 0:
+	if strength <= 0:
 		finished.emit(self)
 	return _damage
 
 func is_active():
-	return power > 0
+	return strength > 0
 
 func stack(other):
-	power += (other as ShieldEffect).power
+	#power.iadd((other as ShieldEffect).power)
 	return true
