b38c4e086ac2aa68c25dcb52190663f312cde4e9
EgerLetov <robur1234@mail.ru>
2024-05-15 16:42:46 +0500

    save

--- strategic_mode/tile_events/collectables/collectable.gd
+++ strategic_mode/tile_events/collectables/collectable.gd
@@ -18,7 +18,8 @@ func _ready():
 	init()
 	assert(item != null, "collectables need their values refefined in seperate script")
 	assert(count != null, "collectables need their values refefined in seperate script")
-
+	sprite.texture = texture
+	
 func activate():
 	game = get_tree().root.get_child(0)
 	inventory = game.strat_map.player.inventory
